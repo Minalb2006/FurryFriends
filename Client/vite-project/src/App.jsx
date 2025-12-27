db091502a426ea80d0b94323f905057be6abbb7f
devyanis05 <shelkegaju700@gmail.com>
2025-12-27 21:55:05 +0530

    Signup and Login Done

--- Client/vite-project/src/App.jsx
+++ Client/vite-project/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import Signup from './Components/Signup.jsx'
+import Login from './Components/Login.jsx'
 
 function App() {
   return (
     <>
-      
+     <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
