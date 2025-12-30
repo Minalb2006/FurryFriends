d5abfd861dfac291e965f719dae7457c148f33a7
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-12-30 12:49:25 +0530

    navbar

--- Client/vite-project/src/App.jsx
+++ Client/vite-project/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Navbar from './Component/Navbar'
 
 function App() {
   return (
     <>
-      
+      <Navbar/>
     </>
   )
 }
 
-export default App
+export default App;
