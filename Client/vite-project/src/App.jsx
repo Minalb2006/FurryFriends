aa38c3e49b0a8f38c9935e8e6eab9201dc0a02be
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-01-02 12:35:31 +0530

    changes

--- Client/vite-project/src/App.jsx
+++ Client/vite-project/src/App.jsx
@@ -11,7 +11,6 @@ function App() {
       <Navbar/>
      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navbar />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
