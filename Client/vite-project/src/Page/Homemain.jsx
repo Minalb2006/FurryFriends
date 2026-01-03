d96cbc8d40b82b101874c09bb49bd3dabc066953
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-01-03 14:55:10 +0530

    delete footer

--- Client/vite-project/src/Page/Homemain.jsx
+++ Client/vite-project/src/Page/Homemain.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Home1 from '../Components/Bhome1'
 import Home2 from '../Components/Bhome2'
 import Home3 from '../Components/Bhome3'
-import Home4 from '../Components/Bhomef'
 
 function Homemain() {
   return (
@@ -10,7 +9,6 @@ function Homemain() {
       <Home1/>
       <Home2/>
       <Home3/>
-      <Home4/>
     </>
   )
 }
